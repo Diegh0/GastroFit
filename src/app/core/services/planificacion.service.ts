61f3ff89d4213f72556c4aa72176815667e37ea3
Diego Fuertes Moreno <dfuertes@AddingTechnology.local>
2025-05-08 16:42:28 +0200

    Cambio en planificacion service

--- src/app/core/services/planificacion.service.ts
+++ src/app/core/services/planificacion.service.ts
@@ -8,37 +8,39 @@ import { firstValueFrom } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class PlanificacionService {
   constructor(private firestore: Firestore, private auth: AuthService) {}
-  
+
   guardarPlanificacion(userId: string, semana: string, planificacion: any): Promise<void> {
-    const ref = doc(this.firestore, `usuarios/${userId}/planificacion/${semana}`);
+    const ref = doc(this.firestore, `users/${userId}/planificacion/${semana}`);
     return setDoc(ref, planificacion);
   }
+
   async getPlanificacion(fechaSemana: string): Promise<PlanificacionSemanal | null> {
     const uid = await this.auth.getUserId();
-    const ref = doc(this.firestore, `usuarios/${uid}/planificacion/${fechaSemana}`);
+    const ref = doc(this.firestore, `users/${uid}/planificacion/${fechaSemana}`);
     const snap = await getDoc(ref);
     return snap.exists() ? (snap.data() as PlanificacionSemanal) : null;
   }
 
   async setPlanificacion(fechaSemana: string, data: PlanificacionSemanal): Promise<void> {
     const uid = await this.auth.getUserId();
-    const ref = doc(this.firestore, `usuarios/${uid}/planificacion/${fechaSemana}`);
+    const ref = doc(this.firestore, `users/${uid}/planificacion/${fechaSemana}`);
     await setDoc(ref, data);
   }
 
   async getSemanasGuardadas(): Promise<string[]> {
     const uid = await this.auth.getUserId();
-    const ref = collection(this.firestore, `usuarios/${uid}/planificacion`);
+    const ref = collection(this.firestore, `users/${uid}/planificacion`);
     const snapshot = await getDocs(ref);
     return snapshot.docs.map(doc => doc.id).sort();
   }
+
   async eliminarPlanificacion(fechaSemana: string): Promise<void> {
     const user = await firstValueFrom(this.auth.user$);
     if (!user) throw new Error('No hay usuario autenticado');
-  
-    const planificacionesRef = collection(this.firestore, `usuarios/${user.uid}/planificacion`);
+
+    const planificacionesRef = collection(this.firestore, `users/${user.uid}/planificacion`);
     const planificacionDoc = doc(planificacionesRef, fechaSemana);
     await deleteDoc(planificacionDoc);
   }
-  
 }
+
